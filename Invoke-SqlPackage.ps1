d83eb959f2cb2edca9d23221182e16d9bea3ca4a
MARCO-K <MARCO-K@users.noreply.github.com>
2020-05-14 15:00:22 +0200

    updated: Invoke-SqlPackage.

--- Invoke-SqlPackage.ps1
+++ Invoke-SqlPackage.ps1
@@ -7,7 +7,10 @@ function Invoke-SqlPackage
  
       .DESCRIPTION
       Invoke the sqlpackage executable and pass the necessary parameters to it
- 
+
+      .PARAMETER $Executable
+      Path to executable SqlPackage.exe
+
       .PARAMETER Action
       Can either be import or export
  
@@ -27,24 +30,25 @@ function Invoke-SqlPackage
       The password for the SQL Server user.
  
       .PARAMETER UseTrustedConnection
-      Should the sqlpackage work with TrustedConnection or not
+      Should the sqlpackage work with TrustedConnection or not.
 
       .PARAMTER Diagnostics
-      Should Diagnostics information be written
+      Should Diagnostics information be written.
 
       .PARAMETER DiagnosticsFile
-      Path to the diagnostics file
+      Path to the diagnostics file.
  
       .PARAMETER FilePath
-      Path to the file, used for either import or export
+      Path to the file, used for either import or export.
 
       .PARAMETER MaxParallelism
-      Degree of Parallelism. Default value is 4
+      Degree of Parallelism. Default value is 4.
  
       .PARAMETER Properties
       Array of all the properties that needs to be parsed to the sqlpackage.exe
  
       .EXAMPLE
+
       $BaseParams = @{
       Executable= path_to_executable
       DatabaseServer = 'Server\Instance'
@@ -67,13 +71,14 @@ function Invoke-SqlPackage
   [CmdletBinding()]
   param (
     [string]$Executable,
-    [ValidateSet('Import', 'Export')][string]$Action, 
+    [ValidateSet('Import', 'Export','Extract','Publish')][string]$Action, 
     [string]$DatabaseServer,
     [string]$DatabaseName,
     [string]$SqlUser,
     [string]$SqlPwd,
     [switch]$UseTrustedConnection,
     [string]$FilePath,
+    [switch]$OverwriteFiles,
     [string[]]$Properties,
     [switch]$Diagnostics,
     [string]$DiagnosticsFile,
@@ -92,11 +97,13 @@ function Invoke-SqlPackage
     Write-PSFMessage -Level Output -Message 'Starting to prepare the parameters for sqlpackage.exe'
     $Params = [Collections.ArrayList]@()
 
+    #Export
     if ($Action -eq 'Export') 
     {
       $null = $Params.Add('/Action:export')
       $null = $Params.Add("/SourceServerName:$DatabaseServer")
       $null = $Params.Add("/SourceDatabaseName:$DatabaseName")
+      $null = $Params.Add("/OverwriteFiles:$OverwriteFiles")
       $null = $Params.Add("/TargetFile:$FilePath")
       $null = $Params.Add('/Properties:CommandTimeout=1200')
       $null = $Params.Add("/MaxParallelism:$MaxParallelism")
@@ -111,17 +118,16 @@ function Invoke-SqlPackage
       {
         $null = $Params.Add("/Diagnostics:$Diagnostics")
         $null = $Params.Add("/DiagnosticsFile:$DiagnosticsFile")
+      }   
+    }
+    #Import
+    elseif ($Action -eq 'Import') 
+    {
+      if (!(Test-Path -Path $FilePath -PathType Leaf -ErrorAction Stop)) 
+      {
+        Write-PSFMessage -Level Critical -Message 'Cannot find file for import' -Exception $FilePath
+        return
       }
-        
-      Remove-Item -Path $FilePath -ErrorAction SilentlyContinue -Force    
-    }
-    else 
-    {
-    if (!(Test-Path -Path $FilePath -PathType Leaf -ErrorAction Stop)) 
-    {
-      Write-PSFMessage -Level Critical -Message 'Cannot find file for import' -Exception $FilePath
-      return
-    }
       $null = $Params.Add('/Action:import')
       $null = $Params.Add("/TargetServerName:$DatabaseServer")
       $null = $Params.Add("/TargetDatabaseName:$DatabaseName")
@@ -139,7 +145,62 @@ function Invoke-SqlPackage
       {
         $null = $Params.Add("/Diagnostics:$Diagnostics")
         $null = $Params.Add("/DiagnosticsFile:$DiagnosticsFile")
-      } 
+      }
+    }
+    #Extract
+    elseif ($Action -eq 'Extract')
+    {
+      $null = $Params.Add('/Action:extract')
+      $null = $Params.Add("/SourceServerName:$DatabaseServer")
+      $null = $Params.Add("/SourceDatabaseName:$DatabaseName")
+      $null = $Params.Add("/OverwriteFiles:$OverwriteFiles")
+      $null = $Params.Add("/TargetFile:$FilePath")
+      $null = $Params.Add('/Properties:CommandTimeout=1200')
+      $null = $Params.Add('/Properties:ExtractReferencedServerScopedElements=False')
+      $null = $Params.Add('/Properties:ExtractUsageProperties=False')
+      $null = $Params.Add('/Properties:IgnoreExtendedProperties=True')
+      $null = $Params.Add('/Properties:IgnorePermissions=True')
+      $null = $Params.Add('/Properties:IgnoreUserLoginMappings=True')
+      $null = $Params.Add("/MaxParallelism:$MaxParallelism")
+
+      if (!$UseTrustedConnection) 
+      {
+        $null = $Params.Add("/SourceUser:$SqlUser")
+        $null = $Params.Add("/SourcePassword:$SqlPwd")
+      }           
+      if ($Diagnostics) 
+      {
+        $null = $Params.Add("/Diagnostics:$Diagnostics")
+        $null = $Params.Add("/DiagnosticsFile:$DiagnosticsFile")
+      }
+    } 
+    #Publish
+    elseif ($Action -eq 'Publish') 
+    {
+      if (!(Test-Path -Path $FilePath -PathType Leaf -ErrorAction Stop)) 
+      {
+        Write-PSFMessage -Level Critical -Message 'Cannot find file for import' -Exception $FilePath
+        return
+      }
+      $null = $Params.Add('/Action:publish')
+      $null = $Params.Add("/TargetServerName:$DatabaseServer")
+      $null = $Params.Add("/TargetDatabaseName:$DatabaseName")
+      $null = $Params.Add("/OverwriteFiles:$OverwriteFiles")
+      $null = $Params.Add("/SourceFile:$FilePath")
+      $null = $Params.Add("/MaxParallelism:$MaxParallelism")
+      $null = $Params.Add('/Properties:CommandTimeout=1200')
+        
+      if (!$UseTrustedConnection) 
+      {
+        $null = $Params.Add("/TargetUser:$SqlUser")
+        $null = $Params.Add("/TargetPassword:$SqlPwd")
+      }   
+        
+      if ($Diagnostics) 
+      {
+        $null = $Params.Add("/Diagnostics:$Diagnostics")
+        $null = $Params.Add("/DiagnosticsFile:$DiagnosticsFile")
+      }
     }
 
     foreach ($item in $Properties) 
